ef34a2fb5503044c3333b9398c679867f430f67d
DhruvBlueMind <69252327+DhruvBhatia0@users.noreply.github.com>
2023-02-18 22:56:43 -0800

    chat chat

--- TFIDF_stuff/chat_bot.py
+++ TFIDF_stuff/chat_bot.py
@@ -5,16 +5,19 @@ import paragraph_picker
 # load_dotenv()
 
 #openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.api_key = 'ENTER YOUR OWN API KEY'
+openai.api_key = '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'
 while True:
     prompt = input("Ask me anything: ")
     info = paragraph_picker.get_information(prompt)
-    prompt = """You are a chat bot. Make sure to only use the following information to answer the users question. Answer truthfully. Make the response sound polite and detailed. If the user asks a personal question, respond and ask them how you can help them:
+    prompt = """You are a chat bot. Make sure to only use the following information to answer the users question. Answer truthfully. Make the response sound polite and detailed and one sentence only. If the user asks a personal question, respond and ask them how you can help them:
     """ + ' ' + info + ' ' + """
-    This is the question: """ + prompt 
+    This is the question: """ + prompt
 
     # print("-----------------")
     # print(prompt)
     # print("-----------------")
     completions = openai.Completion.create(prompt=prompt, engine='text-davinci-002', max_tokens=50)
-    print(completions['choices'][0]['text'].strip())
+    completions = completions['choices'][0]['text'].strip()
+    completions = completions.split('\n')
+    completions = completions[len(completions)-1]
+    print(completions)
