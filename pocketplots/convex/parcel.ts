748e5d9b15065fecda5fde0058e70c98b8347369
victorxheng <victorchengthesecond@gmail.com>
2023-02-18 18:42:20 -0800

    test

--- pocketplots/convex/parcel.ts
+++
@@ -1,6 +0,0 @@
-import { query } from "./_generated/server";
-
-const parcelsAll = query(async ({db}) => {
-    const parcels = await db.query("parcel").order("asc").collect();
-    return parcels;
-})
